c11a3a0f432a00e986ca2d23bb780f48faf0ef2e
Michael Bialowas <mbialowas@rrc.ca>
2022-10-31 14:49:59 -0500

    Added shopping cart styling to header/footer of page template

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,7 +2,12 @@ class CartController < ApplicationController
   def create
     logger.debug("Adding #{params[:id]} to cart.")
     id = params[:id].to_i
-    session[:shopping_cart] << id # push id tonto the end of array
+
+    unless session[:shopping_cart].include?(id)
+      session[:shopping_cart] << id
+      product = Product.find(id)
+      flash[:notice] = " ➕ #{product.name} added to cart."
+    end # push id tonto the end of array
     redirect_to root_path
   end
 
